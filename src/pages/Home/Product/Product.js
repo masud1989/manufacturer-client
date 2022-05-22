9db61b23552c198863164b6e5f13eca772eb7253
masud1989 <mranasrj@gmail.com>
2022-05-22 22:12:59 +0600

    products-show-from-db

--- src/pages/Home/Product/Product.js
+++ src/pages/Home/Product/Product.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import { Button } from 'react-bootstrap';
+import { useNavigate } from 'react-router-dom';
+import './Product.css';
 
-const Product = () => {
+const Product = ({product}) => {
+    const {_id, name, img, description, price, quantity, supplier } = product;
+    const navigate = useNavigate();
+
+    // const handleNavigateToProductDetails = id => {
+    //     navigate(`/product/${_id}`);
+    // }
+
     return (
-        <div>
-            <h1>This is product page</h1>
+        <div className='product'>
+            <img src={img} alt=''/>
+            <h3>{name}</h3>
+            <p>Price: {price} Tk.</p>
+            <p>{description?.slice(0,100)}</p>  
+            <h4>Available in Stock: {quantity}</h4>  
+            <h4>Supplier:{supplier}</h4>  
+            <Button className='btn btn-info'>Product Details</Button>          
+            {/* <Button onClick={ ()=> handleNavigateToProductDetails(_id)} className='btn btn-info'>Product Details</Button>           */}
         </div>
     );
 };
